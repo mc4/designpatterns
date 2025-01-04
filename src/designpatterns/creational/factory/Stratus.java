87bd537a9c9755c0ae476111d873d888e37a34d3
mc4 <mark.e.conley@gmail.com>
2025-01-04 00:25:07 -0500

    stratus concrete class

---
+++ src/designpatterns/creational/factory/Stratus.java
@@ -0,0 +1,10 @@
+package designpatterns.creational.factory;
+
+public class Stratus implements Cloud {
+
+	@Override
+	public void draw() {
+		System.out.println("Drawing Stratus Clouds");
+	}
+	
+}
